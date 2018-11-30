4d0c79f603cd2348ef2138fbc4b38c4c27c64e76
Giovanni Gustavo <giovannis@alunos.utfpr.edu.br>
2018-11-29 23:51:24 -0200

    Descontinuado por erro no start da aplicação, desenvolvimento em php com a mesma ideia de funcionalidade

---
+++ off/build/precache-manifest.c3c9e6e43208cc51a161ed9d4258231d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c4b420f1150aa1960f5",
+    "url": "/static/js/main.3c4b420f.chunk.js"
+  },
+  {
+    "revision": "4194473f5ed8ec45ed72",
+    "url": "/static/js/1.4194473f.chunk.js"
+  },
+  {
+    "revision": "3c4b420f1150aa1960f5",
+    "url": "/static/css/main.835c9eb0.chunk.css"
+  },
+  {
+    "revision": "4194473f5ed8ec45ed72",
+    "url": "/static/css/1.43672d4f.chunk.css"
+  },
+  {
+    "revision": "8921fa42c3d796a843f8d18d51cb3cf9",
+    "url": "/index.html"
+  }
+];
---
+++ off/src/components/Toolbar/Toolbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Toolbar.css';
+
+const toolbar = props => (
+    <header className="toolbar">
+    
+        <nav className="toolbar_nav">
+            <div></div>
+            <div className="toolbar_logo"> 
+                <a href="/">Q-Life</a>
+            </div>
+            
+            <div className="spacer" />
+
+            <div className="toolbar_items">
+                <ul>
+                    <li><a href="/pagina1.js"> Sistema</a></li>
+                    <li><a href="/"> Agendamento</a></li>
+                    <li><a href="/"> Cadastro de Clientes</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
